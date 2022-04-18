e47aa2c2f955267eb1928dcac30ce35aaef00a47
SoHaila NekHat <sohaila.wb@gmail.com>
2022-04-18 06:12:14 +0430

    information about afghanistan added

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Afghanistan, officially the Islamic Emirate of Afghanistan, is a landlocked country located at the crossroads of Central and South Asia.\nCapital: Kabul\n" +
+                "Dialing code: +93\n" +
+                "Area: 652,860 km²\n" +
+                "Population: 38.93 million (2020) World Bank\n" +
+                "Currency: Afghan afghani"+"Government: Unitary provisional theocratic Isla...\n" +
+                "Driving side: right\n" +
+                "Religion: 99.7% Islam (official\u200E); 0.3% Others ...\u200E\n" +
+                "Ethnic groups (2019 unofficial estimates)\nAfghanistan has\n*34 proninces\n*The latest set includes 421 districts\n" +
+                "#Provinces of Afghanistan\n" +
+                "1. kabul\n2. Badghis\n3. Baghlan\n4. Balkh\n5. Bamyan\n6. Badakhshan\n7. Daykundi\n8. Farah\n9. Faryab\n10. Ghazni\n11. Ghor\n12. Helmand\n13. Herat\n14. Jowzjan\n15. Kandahar\n16. Kunduz\n17. Kapisa\n18. Khost\n19. Kunar\n20. Laghman\n21. Logar\n22. Nangharhar\n23. Nimruz\n24. Nuristan\n25. Paktia\n26. Paktika\n" +
+                "27. Panjshir\n28. Parwan\n29. Samangan\n30. Sar-e pol\n31. Takhar\n32. Uruzgan\n33. Wardak\n34. Zabul\nTo view any province's information press the number of province" );
+
+       // int n = 0;
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        switch (n){
+            case 1:
+                System.out.println("");
+                break;
+        }
+
+    }
+}
