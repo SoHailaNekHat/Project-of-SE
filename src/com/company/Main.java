61f68a126e59b05be10bc1d9ee09137e6c058b73
SabrinaHayat <103025292+SabrinaHayat@users.noreply.github.com>
2022-04-19 10:32:23 +0430

    Information Added
    Information about 3 provinces Added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,6 +25,98 @@ public class Main {
             case 1:
                 System.out.println("");
                 break;
+case 20:
+                System.out.println("Laghman (Pashto/Dari: لغمان) is one of the 34 provinces of Afghanistan, located in the eastern part of the country. It has a population of about 502,148,[2] which is multi-ethnic and mostly a rural society. The city of Mihtarlam serves as the capital of the province. In some historical texts the name is written as "Lamghan" or as "Lamghanat".
+Capital
+Mihtarlam
+Government
+ • Governor
+Zain-Ul-Abideen
+ • Deputy Governor
+Saeed Ahmad[1]
+Area
+ • Total
+3,842.6 km2 (1,483.6 sq mi)
+Population (2021)[2]
+ • Total
+502,148
+ • Density
+130/km2 (340/sq mi)
+Time zone
+UTC+4:30 (Afghanistan Time)
+ISO 3166 code
+AF-LAG
+Main languages
+
+Pashto
+Pashayi
+the total population of the province is about 502,148, which is multi-ethnic and mostly a rural society.[2] According to the Naval Postgraduate School, as of 2010 the ethnic groups of the province are as follows: 51.3% Pashtun, 27% Pashai and Nuristani (Kata), and 21.7% Tajik.[29][30] The people of Laghman are overwhelmingly Sunni Muslim.
+");
+                break;
+case 21:
+                System.out.println("Logar (Pashto: لوګر; Dari: لوگر) meaning Greater Mountain ( لوې غر) is one of the 34 provinces of Afghanistan located in the eastern section of the country. It is divided into 7 districts and contains hundreds of villages. Ahmadzai Pashtuns are dominant in this region. Puli Alam is the capital of the province. As of 2021, Logar has a population of approximately 442,037.[3] It is a multi-ethnic tribal society, with a Tajik and Pashtun majority.
+
+Capital
+Puli Alam
+Government
+ • Governor
+Maulvi Inayatullah[1]
+ • Deputy Governor
+Maulvi Inamullah Salahuddin[2]
+ • Police Chief
+Mohammaduddin Shah Mukhtab[2]
+Area
+ • Total
+3,879.8 km2 (1,498.0 sq mi)
+Population (2021)[3]
+ • Total
+442,037
+ • Density
+110/km2 (300/sq mi)
+Time zone
+UTC+4:30 (Afghanistan Time)
+ISO 3166 code
+AF-LOG
+Main languages
+Dari
+Pashto
+
+the total population of Logar province is about 442,037.[3] Logar is dominantly Sunni, multi-ethnic and a tribal society.Pashtuns and Tajiks make up the majority population of Logar province, while Hazaras form a minority.[4][13][15]
+
+7.2 percent of the population lived below the national poverty line, the lowest of all of Afghanistan's provinces.[16]
+");
+                break;
+case 22:
+                System.out.println("Nangarhār (Pashto: ننګرهار; Dari: ننگرهار) also called Nangrahar or Ningrahar, is one of the 34 provinces of Afghanistan, located in the eastern part of the country and bordering Logar, Kabul, Laghman and Kunar provinces as well as having an international border with Pakistan. It is divided into 22 districts and has a population of about 1,735,531,[3] the third highest of the country's 34 provinces. The city of Jalalabad is the capital of Nangarhar province.
+Capital
+Jalalabad
+Government
+ • Governor
+Hajji Gul Mohammad[1]
+ • Deputy Governor
+Maulvi Niaz Mohammad Wahaj[2]
+ • Police Chief
+Neda Mohammad
+Area
+ • Total
+7,727 km2 (2,983 sq mi)
+Population (2021)[3]
+ • Total
+1,735,531
+ • Density
+220/km2 (580/sq mi)
+Time zone
+UTC+4:30 (Afghanistan Time)
+ISO 3166 code
+AF-NAN
+Main languages
+Pashto, Dari
+
+the population of the province is around 1,735,531.[3] Over 90% of the population is Pashtun and the remaining is made up of Pashais, Tajiks, Arabs, and other ethnic groups.[16] The 18th edition Ethnologue states on p. 48 that Nangarhar is the center of the (smaller) Northern Pashto language in Afghanistan. Only 1 in 5 Afghan Pashtuns use the Northern variety.
+");
+                break;
+
+
         }
 
     }
