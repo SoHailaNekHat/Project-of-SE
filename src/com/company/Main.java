748ae7bc6dd3e9af8bedf7b93d6580fa742049b3
Zahidasafi <103043666+zahidasafi@users.noreply.github.com>
2022-04-19 10:09:16 +0430

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,8 +23,36 @@ public class Main {
         int n= sc.nextInt();
         switch (n){
             case 1:
+                System.out.println("Badghis Province is located in the isolated hills of northwestern Afghanistan and shares its borders with Herat, Ghor, and Faryab provinces as well as Turkmenistan. The province is dominated by the Murghab River in the north and the Hari-Rud River in the south. Badghis remains extremely underdeveloped, especially outside the provincial capital of Qalay-i-Naw. The province is inhabited by Tajiks who are thought to make up 62 per cent of the population with Pashtuns making up approximately 28 percent.
+
+Currently, the security situation has deteriorated in the northern districts of Ghormach and Murghab, both of which are heavily populated by Pashtuns and sympathetic to the local Taliban. The strategic “ring-road” highway that connects Kabul to Kandahar and Herat also has an uncompleted northern leg, a portion of which cuts through Badghis. The roadway in Badghis is currently unpaved and all efforts to begin work on it has faced delays due to security issues. Until 2008, there was no permanent Afghan Army presence in the province, and most provincial police units were based in the capital with only 40-50 security personnel stationed in each of the remote districts.
+
+Dr. Jamaluddin Ishaq, an ethnic Tajik, but reported to be a member of a Pashtun national party. He was appointed to be the Governor of Badghis in September 2015 by President Ashraf Ghani. This is believed to be his first position in the Afghan government. Upon taking office, Ishaq laid out his top priorities: improving security, procuring electricity from Turkmenistan, and clean drinking water in the provincial capital of Qala-i-Naw.13
+
+The Taliban's shadow governor for Badghis was killed in October 2015 during a confrontation between Afhgan security and Taliban fighters.14");
+                break;
+case 2:
                 System.out.println("");
                 break;
+case 3:
+                System.out.println("Baghlan Province is located in northeastern Afghanistan has an area of 21,112 sq. km (8,151 sq. miles) and a population estimated at 762, 500. The northern part of Baghlan contains slopes of the Hindu Kush mountain range crossed by the Robatak, Barabi, Khawak and Salang Passes. Baghlan maintains an ethnically diverse population which includes Tajiks, Pashtuns, Uzbeks, Hazara and Tartars.
+
+Agriculture remains a main source of labor for Baghlan residents particularly in northern parts of the province where grapes, pistachio nuts, pomegranates and sugar beets are grown. Since the 1940s, Baghlan has served as a big producer of Afghan sugar with a large refining facility located near Pul-i-Khumri. Decades of war, instability and drought severely damaged Baghlan’s economic capabilities.
+
+Recent efforts by NATO and the international community have rehabilitated a large sugar factory near Pul-i-Khumri with some sugar refining operations beginning in 2006. A second sugar factory was finished the following November, but its inauguration was marred after a suicide-bomber detonated himself among the opening ceremony, killing at least 68 people; mostly young children tasked with singing in a choir to the visiting Members of Parliament who were visiting the facility. Five visiting MP’s, including, former Commerce Minister Sayed Mustafa Kazemi, died in the attack.
+
+Baghlan has suffered from ineffective governance since 2001 with nine different governors attempting to bring stability and security to the province. The current governor, Munshi Abdul Majeed, replaced Haji Mohammad Akbar Barakzai, who was appointed in January 2009. A recent series of suicide bombings, factional fighting, and criminal activity has continued to plague Baghlan, including its provincial capital, Pul-i-Khumri. Majeed is a former Hezb-i-Islami commander and native of Baghlan province who has served as the governor of Badakshan for several years before replacing Akbar Barakzai.");
+                break;
+case 4:
+                System.out.println("Balkh province is located in northern Afghanistan and is known for its fertile land and high potential in the agricultural sector. Mazar-i-Sharif, located in Balkh, is Afghanistan’s fourth largest city and by far the most important urban center of the northern region.  Given the relative security and political stability in Balkh, development and construction projects like irrigation rehabilitation has occurred over the past five years.  A substantial amount of almond and apricot orchards have been planted and maintained since the irrigation overhaul began.
+
+Balkh has a tradition of high educational standards and has a comparatively high literacy, also for women, and Balkh is home to several universities including Balkh University, the second largest university in Afghanistan.
+
+Balkh is ethnically diverse, including substantial Tajik, Hazara, Pashtuns, Arab, Uzbek, Turkomen, and Sunni Hazara (Kawshi) communities.  The current governor, Ustad Atta Noor Mohammad, is a Tajik with a concentrated political power-base in Balkh and served as the provincial head since 2004. Although largely secure, recent insurgent incursions into Balkh are increasing, and Taliban affiliated insurgent fronts are gaining momentum in the northeast (Baghlan-Kunduz corridor) and the northwest (Badghis-Faryab corridor).
+
+Taliban Shadow Governor: Mawlavi Naser (captured March 2016) - Mawlavi Naser was captured by security forces near Khenjan district in March 2016. His replacement is not yet known.");
+                break;
+
         }
 
     }
